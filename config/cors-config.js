9479f57901d0750159a1c3d2bcb874e0a2977f67
Gabriel Menendez <gabriel.menendez@mediamonks.com>
2020-03-03 17:57:35 -0300

    Add cors config

---
+++ config/cors-config.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, auth"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  next();
+};
